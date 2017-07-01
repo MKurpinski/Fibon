9fc288385bd07afe92539ddaac2adfcda8d134e7
Michał Kurpiński <michal.kurpinski96@gmail.com>
2017-07-01 10:50:23 +0200

    Connect RabbitMq

--- Fibon.Api/Controllers/FibonacciController.cs
+++ Fibon.Api/Controllers/FibonacciController.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Semantics;
+using RawRabbit;
 
 namespace Fibon.Api.Controllers
 {
     [Route("[controller]")]
     public class FibonacciController:Controller
     {
+        private readonly IBusClient _busClient;
+        public FibonacciController(IBusClient busClient)
+        {
+            _busClient = busClient;
+        }
         [HttpGet("{number}")]
         public IActionResult Get(int number)
         {
