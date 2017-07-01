239fa97aef5131722965447727366e25b5122068
Michał Kurpiński <michal.kurpinski96@gmail.com>
2017-07-01 13:38:32 +0200

    Creating docker images

---
+++ Scripts/buildDockerImages.sh
@@ -0,0 +1,2 @@
+docker build -t mkurpinski/fibon-api ./Fibon.Api
+docker build -t mkurpinski/fibon-service ./Fibon.Service
---
+++ Scripts/dotnetPublish.sh
@@ -0,0 +1,2 @@
+dotnet publish ./Fibon.Api -c Release -o ./Bin/Docker
+dotnet publish ./Fibon.Sercive -c Release -o ./Bin/Docker
